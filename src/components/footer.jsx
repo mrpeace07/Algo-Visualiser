96d1ea8445fc68f574ca1ca64c56fa0b51a0beb0
Nishanth S <140835316+mrpeace07@users.noreply.github.com>
2024-08-02 22:52:10 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Footer = () => (
     <footer>
         <p>
-            Created with <i className="fa fa-heart" style={{ color: "#FF5252" }}></i> by
+            Created with <i className="fa fa-heart" style={{ color: "#FF5252" }}></i> by 
             <a 
                 href="https://github.com/mrpeace07" 
                 target="_blank" 
                 rel="noopener noreferrer" 
                 className="link"
             >
-                Nishanth S
+                 Mr.Peace
             </a>
         </p>
     </footer>
