8038a610d1faf5837e951eb85fb8c87cda2cb640
Nishanth S <140835316+mrpeace07@users.noreply.github.com>
2024-08-08 14:07:02 +0530

    Update index.js

--- src/components/algorithms/index.js
+++ src/components/algorithms/index.js
@@ -3,6 +3,5 @@ import { insertionSort } from './insertionSort';
 import { selectionSort } from './selectionSort';
 import { mergeSort } from './mergeSort';
 import { quickSort } from './quickSort';
-import { heapSort } from './heapSort';
 
-export { bubbleSort, insertionSort, selectionSort, mergeSort, quickSort, heapSort };
+export { bubbleSort, insertionSort, selectionSort, mergeSort, quickSort };
